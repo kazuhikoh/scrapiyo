0a77f444b27e2f3c884a090ccaa7740be73ecdac
kazuhikoh <kazuhiko.hrmt@gmail.com>
2019-03-27 18:43:17 +0900

    stdin

--- scrapiyo.sh
+++ scrapiyo.sh
@@ -1,60 +1,12 @@
 #!/bin/bash
 
-usage(){
-  cat <<GUMOR
-Usage:
-  $0 -h
-  $0 [-d filepath] [URL] 
-  
-Description:
-  ⊂(＾ω＾)⊃
-
-Options:
-  -h help
-  -d diff-only output
-GUMOR
-}
-
-# diff target
-# if not empty, output only when sorapiyo changes.
-DIFF_FILEPATH=''
-
-# options
-while getopts hd: opts
-do
-  case $opts in
-  h)
-    usage
-  exit 0
-  ;;
-  d)
-    DIFF_FILEPATH="$OPTARG"
-  ;;
-  \?)
-    exit 1
-  esac
-done
-shift $((OPTIND - 1))
-
-readonly DEFAULT_URL=http://piyo.fc2.com/soramaru/
-readonly URL=$([ ! -z "$1" ] && echo "$1" || echo "$DEFAULT_URL")
-
-html=$(curl -Ls "$URL")
-
-# Validation
-html_l1=$(echo "$html" | head -n 1)
-if [[ ! "$html_l1" =~ ^\<!DOCTYPE ]]; then
-  echo >&2 "Fetching html failed: ${URL}"
-  exit 1
-fi
-
 # To output MESSAGE keeping original line break,
 # xmllint needs to receive a html that contains line-break character (\n) at same position as br tags.
 # Because, xpath statement '/text()' extract ONLY text. 
 # This formatting consist of following steps:
 # 1. Remove all line-break after <br>
 # 2. Append line-break after <br>
-html="$(echo $html | sed -e 's/<br[ /]*>/<br>\n/g')"
+html="$(cat - | sed -e 's/<br[ /]*>/<br>\n/g')"
 
 MESSAGE="$(echo "$html" | xmllint --xpath "//div[@class='article'][position()=1]//div[@class='article_box']/*[1]/div[@class='message_body']/text()" --html - 2>/dev/null | tr -d '\t')"
 
